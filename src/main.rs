7d4ec283415dc735844b5e4edb33899fcf0e0bd6
Ayman <168225570+aymnsk@users.noreply.github.com>
2025-06-18 00:02:39 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
 
+mod processor;
+mod ui;
+
+use crate::ui::DetectorApp;
+use eframe::{egui, NativeOptions};
+
+fn main() {
+    let options = NativeOptions {
+        decorated: true,
+        transparent: true,
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
+        ..Default::default()
+    };
+    
+    eframe::run_native(
+        "Human Detector",
+        options,
+        Box::new(|_cc| Box::new(DetectorApp::default())),
+    );
+}
